72063f18f794cae91bfd19c129cb25214c6542dd
Stanislas Rolland <stanislas.rolland@sjbr.ca>
2026-07-23 10:12:23 -0400

    [BUGFIX] Re-introduce ext_emconf.php files
    Resolves: #111

---
+++ ext_emconf.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Extension Manager configuration file for ext "static_info_tables".
+ */
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Static Info Tables',
+    'description' => 'Data and API for countries, languages and currencies for use in TYPO3 CMS.',
+    'category' => 'misc',
+    'version' => '14.3.0',
+    'state' => 'stable',
+    'author' => 'Stanislas Rolland/René Fritz',
+    'author_email' => 'typo3AAAA@sjbr.ca',
+    'author_company' => 'SJBR',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '14.3.0-14.3.99',
+            'extbase' => '14.3.0-14.3.99',
+            'extensionmanager' => '14.3.0-14.3.99'
+        ]
+    ],
+    'autoload' => [
+        'psr-4' => [
+        	'SJBR\\StaticInfoTables\\' => 'Classes'
+        ]
+    ]
+];
